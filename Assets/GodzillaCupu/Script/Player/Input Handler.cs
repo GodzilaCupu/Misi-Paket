af90b9dc0c013ba82e0057e78139abc8381d2b12
GodzillaCupu <prasetyo.djufri@gmail.com>
2023-12-06 05:31:24 +0700

    Changelog : - Create Input System Property

---
+++ Assets/GodzillaCupu/Script/Player/Input Handler.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GodzillaCupu.Player
+{
+    public class InputHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+}
